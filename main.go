fc7436b6b5bb7cb525fade9cc840924e6fff1382
Alec Aivazis <alec@aivazis.com>
2019-11-15 14:19:51 -0800

    jeeves is fault tolerant :flex:

--- main.go
+++ main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
+	defer func() {
+		// if the main goroutine finished with a panic
+		if r := recover(); r != nil {
+			// keep the bot running
+			bot.Start()
+
+		}
+	}()
+
 	bot.Start()
 }
