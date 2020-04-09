d36560769e16876ab11c16f2f418336b532fa211
Alec Aivazis <alec@aivazis.com>
2020-04-09 01:42:29 -0700

    added root command

--- companion/src/cmd.lua
+++ companion/src/cmd.lua
@@ -8,6 +8,11 @@ function JeevesAddon:ParseCmd(input)
         return JeevesAddon:RootCmd()
     end
 
+    -- /jeeves export
+    if input == "export" then
+        return JeevesAddon:ExportCmd()
+    end
+
 
     -- we did not recognize the command
     print("Unrecognized command: \"" .. input .."\".  Please try again.")
@@ -15,6 +20,12 @@ end
 
 -- a command with no inputs
 function JeevesAddon:RootCmd()
-    -- open the UI
-    JeevesUI.Show()
+    print("Jeeves Companion v0.0.0")
+    print("|cFF80FF80/jeeves export|r - |cFFFF8080export the changes your inventories|r")
 end
+
+-- the command to export the delta between the last known inventory for this 
+-- bank and the current inventory
+function JeevesAddon:ExportCmd()
+    print("Exporting inventory...")
+end 
