80029acb5552af0b730b3062b37ca0fc7bc86881
Alec Aivazis <alec@aivazis.com>
2020-08-31 14:38:32 -0700

    load secrets

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import (
 	"os"
 
 	"github.com/AlecAivazis/jeeves/bot"
+	"github.com/AlecAivazis/jeeves/config"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	// if we aren't running locally
+	if !config.LocalMode {
+		// load config values from google secrets
+		if err := config.LoadSecrets(); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+	}
+
 	// create an instance of jeeves we can run
 	jeeves, err := bot.New()
 	if err != nil {
